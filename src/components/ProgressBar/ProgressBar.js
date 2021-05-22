7eb643749b4c367f7986166f216cd28ecb113132
skulsta <sti.sku@gmail.com>
2021-05-22 13:46:56 +0200

    Border radius

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,31 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper>
+      <Bar style={{ "--width": value + "%" }}></Bar>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  height: 8px;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 4px;
+  /* border radius when the bar is close to full */
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  height: 100%;
+  background-color: ${COLORS.primary};
+  width: var(--width);
+  border-radius: 4px 0 0 4px;
+`;
 
 export default ProgressBar;
